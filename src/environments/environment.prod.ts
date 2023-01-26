28989bef6a45d7f3649adc93f23002de45e20c66
arturozgz <123074970+arturozgz@users.noreply.github.com>
2023-01-26 10:51:12 +0100

    Subir codigo

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDtIK_mUlGkYW2cT9aF0Jg1IBK4YwtWXR0",
+    authDomain: "test-df79c.firebaseapp.com",
+    projectId: "test-df79c",
+    storageBucket: "test-df79c.appspot.com",
+    messagingSenderId: "636179742708",
+    appId: "1:636179742708:web:ffd2aee741a8adfa0503a1",
+    measurementId: "G-4CPPDFSCNJ"
+  }
+
+};
